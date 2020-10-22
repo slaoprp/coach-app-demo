7fc99c0f0cd2cb80bc2b0e6c6894030ecc38b55d
sith norvang <sith.norvang@gmx.com>
2020-10-22 22:26:01 +0200

    vue is a bomb

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ import { createApp } from 'vue';
 import router from './router.js';
 import store from './store/index.js';
 import App from './App.vue';
+import BaseCard from './components/UI/BaseCard.vue'
+import BaseBadge from './components/UI/BaseBadge.vue'
+import BaseButton from './components/UI/BaseButton.vue'
+
 
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+
+app.component('base-card', BaseCard);
+app.component('base-badge', BaseBadge);
+app.component('base-Button', BaseButton);
 
 app.mount('#app');
