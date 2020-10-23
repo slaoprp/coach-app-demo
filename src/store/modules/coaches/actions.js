1e960810728ced7bc21254309f6ce2a807d87dc3
sith norvang <sith.norvang@gmx.com>
2020-10-24 00:06:43 +0200

    automatic refresh & force refresh

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -26,7 +26,10 @@ export default {
             id: userId
         })
     },
-    async loadCoaches(context) {
+    async loadCoaches(context, payload) {
+        if ( !payload.forceRefresh && !context.getters.shouldUpdate) {
+            return;
+        }
         const response = await fetch(
             `https://mrcoach-16fbc.firebaseio.com/coaches.json`
         );
@@ -51,5 +54,6 @@ export default {
             coaches.push(coach);
         }
         context.commit('setCoaches', coaches);
+        context.commit('setFetchTimestamp');
     }
 };
