c14c0727e16370879feef6d451c700c1dc158757
sith norvang <sith.norvang@gmx.com>
2020-10-23 22:36:10 +0200

    dialog alert fetch

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -25,5 +25,30 @@ export default {
             ...coachData,
             id: userId
         })
+    },
+    async loadCoaches(context) {
+        const response = await fetch(
+            `https://mrcoach-16fbc.firebaseio.com/coaches.json`
+        );
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            const error = new Error(responseData.message || 'Failed to fetch!');
+            throw error;
+        }
+
+        const coaches = [];
+
+        for (const key in responseData) {
+            const coach = {
+                firstName: responseData[key].firstName,
+                lastName: responseData[key].lastName,
+                description: responseData[key].description,
+                hourlyRate: responseData[key].hourlyRate,
+                areas: responseData[key].areas         
+            };
+            coaches.push(coach);
+        }
+        context.commit('setCoaches', coaches);
     }
 };
