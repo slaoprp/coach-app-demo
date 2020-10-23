5b6e891c70c722d3f917cf2da942db109738bb5b
sith norvang <sith.norvang@gmx.com>
2020-10-24 00:31:07 +0200

    not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <section>
+        <base-card>
+            <h2>Page not found</h2>
+            <p>Redirect to  <router-link to="/coaches">home</router-link> </p>
+        </base-card>
+    </section>
+</template>
