84b2b696ed1d432c9d436a7d227346784f93e0d1
sith norvang <sith.norvang@gmx.com>
2020-10-24 00:25:55 +0200

    nice transition

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,25 +1,27 @@
 <template>
-    <section>
-        <base-card>
-            <h2>{{ fullName }}</h2>
-            <h3>${{ rate }}</h3>
-        </base-card>
-    </section>
-    <section>
-        <base-card>
-            <header>
-                <h2>Interested ? Reach out now !</h2>
-                <base-button>Contact</base-button>
-            </header>
-            <router-view></router-view>
-        </base-card>
-    </section>
-    <section>
-        <base-card>
-            <base-badge v-for="area in areas" :key="area" :type="areas" :title="area"></base-badge>
-            <p>{{ description }}</p>
-        </base-card>
-    </section>
+    <div>
+        <section>
+            <base-card>
+                <h2>{{ fullName }}</h2>
+                <h3>${{ rate }}</h3>
+            </base-card>
+        </section>
+        <section>
+            <base-card>
+                <header>
+                    <h2>Interested ? Reach out now !</h2>
+                    <base-button>Contact</base-button>
+                </header>
+                <router-view></router-view>
+            </base-card>
+        </section>
+        <section>
+            <base-card>
+                <base-badge v-for="area in areas" :key="area" :type="areas" :title="area"></base-badge>
+                <p>{{ description }}</p>
+            </base-card>
+        </section>
+    </div>
 </template>
 
 <script>
