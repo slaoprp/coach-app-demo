45188d86ac5ce1c7da44fc96275bc71b271bfa95
sith norvang <sith.norvang@gmx.com>
2020-10-23 17:57:22 +0200

    isCoach

--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,16 @@ const store =  createStore({
     modules: {
         coaches: coachesModule,
         requests: requestsModule
+    },
+    state() {
+        return {
+            userId: 'c3'
+        };
+    },
+    getters: {
+        userId(state) {
+            return state.userId
+        }
     }
 });
 
