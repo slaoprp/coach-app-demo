ca28e60226198b6c1c79d33c1c998eef3d0c5072
sith norvang <sith.norvang@gmx.com>
2020-10-24 21:17:51 +0200

    optimisation components async

--- src/router.js
+++ src/router.js
@@ -1,14 +1,38 @@
+import { defineAsyncComponent } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
-import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
-import UserAuth from './pages/auth/UserAuth.vue'
+// import CoachDetail from './pages/coaches/CoachDetail.vue';
+// import CoachesList from './pages/coaches/CoachesList.vue';
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import ContactCoach from './pages/requests/ContactCoach.vue';
+// import RequestsReceived from './pages/requests/RequestsReceived.vue';
+// import UserAuth from './pages/auth/UserAuth.vue'
 import NotFound from './pages/NotFound.vue';
 import store from './store/index.js';
+
+const CoachDetail = defineAsyncComponent(() => 
+  import('./pages/coaches/CoachDetail.vue')
+);
+
+const CoachesList = defineAsyncComponent(() => 
+  import('./pages/coaches/CoachesList.vue')
+);
+
+const CoachRegistration = defineAsyncComponent(() => 
+  import('./pages/coaches/CoachesRegistration.vue')
+);
+
+const ContactCoach = defineAsyncComponent(() => 
+  import('./pages/requests/ContactCoach.vue')
+);
+
+const RequestsReceived = defineAsyncComponent(() => 
+  import('./pages/requests/RequestsReceived.vue')
+);
+
+const UserAuth = defineAsyncComponent(() => 
+  import('./pages/auth/UserAuth.vue')
+);
 
 const router = createRouter({
   history: createWebHistory(),
