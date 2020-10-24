03e581c940fb43ab7f794534ee5d7144e4258e74
sith norvang <sith.norvang@gmx.com>
2020-10-24 20:53:33 +0200

    fullstack authentification

--- src/router.js
+++ src/router.js
@@ -8,25 +8,36 @@ import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import UserAuth from './pages/auth/UserAuth.vue'
 import NotFound from './pages/NotFound.vue';
+import store from './store/index.js';
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/', redirect: '/coaches' },
-        { path: '/coaches/', component: CoachesList },
-        { 
-            path: '/coaches/:id', 
-            component: CoachDetail, 
-            props: true,
-            children: [
-                { path: 'contact', component: ContactCoach},
-            ] 
-        },
-        { path: '/register', component: CoachRegistration },
-        { path: '/requests', component: RequestsReceived },
-        { path: '/auth', component: UserAuth },
-        { path: '/:notFound(.*)', component: NotFound },
-    ]
-})
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches/', component: CoachesList },
+    { 
+      path: '/coaches/:id', 
+      component: CoachDetail, 
+      props: true,
+      children: [
+          { path: 'contact', component: ContactCoach},
+      ] 
+    },
+    { path: '/register', component: CoachRegistration, meta: { requiresAuth: true}  },
+    { path: '/requests', component: RequestsReceived, meta: { requiresAuth: true} },
+    { path: '/auth', component: UserAuth, meta: { requiresUnAuth: true} },
+    { path: '/:notFound(.*)', component: NotFound },
+  ]
+});
+
+router.beforeEach(function(to, _, next) {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/auth');
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+    next('coaches');
+  } else {
+    next();
+  }
+});
 
 export default router
--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,8 +1,29 @@
+let timer;
+
 export default {
   async login(context, payload) {
-    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDiFabKnoiYX2t1mpFuOEZuMaUM9n19ExA', {
-      method: 'POST',
-      body: JSON.stringify({
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login'
+    });
+  },
+  async signup(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup'
+    });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDiFabKnoiYX2t1mpFuOEZuMaUM9n19ExA';
+    if (mode === 'signup') {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDiFabKnoiYX2t1mpFuOEZuMaUM9n19ExA';
+    }
+    const response = await fetch(
+      url, 
+      {
+        method: 'POST',
+        body: JSON.stringify({
           email: payload.email,
           password: payload.password,
           returnSecureToken: true
@@ -17,45 +38,59 @@ export default {
       throw error;
     }
 
-    console.log(responseData);
+    const expiresIn = responseData.expiresIn * 1000;
+    // const expiresIn = 5000;
+    const expirationDate = new Date().getTime() + expiresIn; 
+
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
+    localStorage.setItem('tokenExpiration', expirationDate);
+
+    timer = setTimeout(function() {
+      context.dispatch('autoLogout');
+    }, expiresIn);
+
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
     });
   },
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
 
-  async signup(context, payload) {
-      const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDiFabKnoiYX2t1mpFuOEZuMaUM9n19ExA', {
-        method: 'POST',
-        body: JSON.stringify({
-            email: payload.email,
-            password: payload.password,
-            returnSecureToken: true
-        })
-      });
+    const expiresIn = +tokenExpiration - new Date().getTime();
 
-      const responseData = await response.json();
+    if (expiresIn < 0) {
+      return
+    }
 
-      if (!response.ok) {
-        console.log(responseData)
-        const error = new Error(responseData.message || 'Failed to authenticate. Check your signup data');
-        throw error;
-      }
+    timer = setTimeout(function() {
+      context.dispatch('logout');
+    }, expiresIn)
 
-      console.log(responseData);
+    if (token && userId) {
       context.commit('setUser', {
-        token: responseData.idToken,
-        userId: responseData.localId,
-        tokenExpiration: responseData.expiresIn
-      });
+        token: token,
+        userId: userId,
+      })
+    }
   },
-
   logout(context) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('tokenExpiration');
+
+    clearTimeout(timer);
+
     context.commit('setUser', {
       token: null,
       userId: null,
-      tokenExpiration: null,
     })
+  },
+  autoLogout(context) {
+    context.dispatch('logout');
+    context.commit('setAutoLogout');
   }
 };
